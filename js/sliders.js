8e5eebce8dbc4c0eff31c5f112b4a66217c4474e
Erin Singer <erinsinger93@gmail.com>
2016-04-30 23:53:42 -0700

    making js more consistent

--- js/sliders.js
+++ js/sliders.js
@@ -6,23 +6,21 @@ $(function() {
     value: 5,
     slide: function(event, ui) {
       $(this).prev().text(ui.value);
-      console.log(ui.value);
-      console.log($(this).prev());
     }
   });
     $(this).prev().text($(".slider").slider("value"));
 });
 
 $(function() {
-  $( "#slider-range" ).slider({
+  $( ".slider_range" ).slider({
     range: true,
     min: 0,
     max: 24,
-    values: [8, 15],
+    values: [0, 24],
     slide: function(event, ui) {
-      $("#amount").val(ui.values[0] + ":00 - " + ui.values[1] + ":00");
+      $(this).prev().text(ui.values[0] + ":00 - " + ui.values[1] + ":00");
     }
   });
-  $( "#amount" ).val($("#slider-range").slider("values", 0) +
-    ":00 - " + $("#slider-range").slider("values", 1) + ":00");
+  $(this).prev().text($(".slider_range").slider("values", 0) +
+    ":00 - " + $(".slider_range").slider("values", 1) + ":00");
 });
