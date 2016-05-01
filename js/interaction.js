569c6cc5f9c9638b8d935e056b4fd24c3e126c0b
Patrick Briggs <patkbriggs@gmail.com>
2016-04-30 22:48:44 -0700

    Added data file and cleaned up JS a bit

--- js/interaction.js
+++ js/interaction.js
@@ -1,46 +1,56 @@
 
-// Set up size
+// Declaring constants
 var width = 750,
   height = width;
 
-// Set up projection that map is using
-var projection = d3.geo.mercator()
-  .center([-122.433701, 37.767683]) // San Francisco, roughly
-  .scale(225000)
-  .translate([width / 2, height / 2]);
-// This is the mapping between <longitude, latitude> position to <x, y> pixel position on the map
-// projection([lon, lat]) returns [x, y]
 
-// Add an svg element to the DOM
-var svg = d3.select(".vis_container").append("svg")
-  .attr("width", width)
-  .attr("height", height);
+function createMap() {
+  // Set up projection that map is using
+  var projection = d3.geo.mercator()
+    .center([-122.433701, 37.767683]) // San Francisco, roughly
+    .scale(225000)
+    .translate([width / 2, height / 2]);
+  // This is the mapping between <longitude, latitude> position to <x, y> pixel position on the map
+  // projection([lon, lat]) returns [x, y]
 
-// Add svg map at correct size, assumes map is saved in a subdirectory called "data"
-svg.append("image")
-          .attr("width", width)
-          .attr("height", height)
-          .attr("xlink:href", "../data/sf-map.svg");
+  // Add an svg element to the DOM
+  var svg = d3.select(".vis_container").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-var originX = 200;
-var originY = 200;
-var circleRadius = 100;
-var circle = svg.append("circle").attr({
-    cx: originX,
-    cy: originY,
-    r: circleRadius,
-    fill: "white",
-    stroke: "black"
-});
-
-function updateRadius(nRadius) {
-  d3.select("#radiusSlider").property("value", nRadius);
-
-  // update the circle radius
-  svg.selectAll("circle")
-    .attr("r", nRadius);
+  // Add svg map at correct size, assumes map is saved in a subdirectory called "data"
+  svg.append("image")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("xlink:href", "../data/sf-map.svg");
 }
 
-d3.select("#radiusSlider").on("input", function() {
-  updateRadius(+this.value);
-});
+
+
+
+
+
+
+// --- Playing with circles ---
+// var originX = 200;
+// var originY = 200;
+// var circleRadius = 100;
+// var circle = svg.append("circle").attr({
+//     cx: originX,
+//     cy: originY,
+//     r: circleRadius,
+//     fill: "white",
+//     stroke: "black"
+// });
+
+// function updateRadius(nRadius) {
+//   d3.select("#radiusSlider").property("value", nRadius);
+
+//   // update the circle radius
+//   svg.selectAll("circle")
+//     .attr("r", nRadius);
+// }
+
+// d3.select("#radiusSlider").on("input", function() {
+//   updateRadius(+this.value);
+// });
