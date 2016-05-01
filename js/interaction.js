74b202c8eb2d2c29b7c57553d4a104cbc92695eb
Patrick Briggs <patkbriggs@gmail.com>
2016-04-30 23:18:47 -0700

    Load in all data and display on map

--- js/interaction.js
+++ js/interaction.js
@@ -2,17 +2,20 @@
 // Declaring constants
 var width = 750,
   height = width;
+var circleRadius = 3;
 
 
-function createMap() {
+function getMapProjection() {
   // Set up projection that map is using
-  var projection = d3.geo.mercator()
+  return d3.geo.mercator()
     .center([-122.433701, 37.767683]) // San Francisco, roughly
     .scale(225000)
     .translate([width / 2, height / 2]);
   // This is the mapping between <longitude, latitude> position to <x, y> pixel position on the map
   // projection([lon, lat]) returns [x, y]
+}
 
+function createMapBaseImage() {
   // Add an svg element to the DOM
   var svg = d3.select(".vis_container").append("svg")
     .attr("width", width)
@@ -23,26 +26,44 @@ function createMap() {
     .attr("width", width)
     .attr("height", height)
     .attr("xlink:href", "../data/sf-map.svg");
+
+  return svg;
 }
 
-function loadCrimeData() {
+function createCircleAtLocation(svg, coords) {
+  var circle = svg.append("circle").attr({
+      cx: coords[0],
+      cy: coords[1],
+      r: circleRadius,
+      fill: "white",
+      stroke: "black"
+  });
+  return circle;
+}
+
+function loadCrimeData(callback) {
   d3.json("../data/scpd_incidents.json", function(dataObject) {
     data = dataObject["data"];
-    // var circle = svg.append("circle").attr({
-    //     cx: originX,
-    //     cy: originY,
-    //     r: circleRadius,
-    //     fill: "white",
-    //     stroke: "black"
-    // });
-    console.log("data loaded");
+    callback(data);
+  });
+}
+
+
+function createMap() {
+  var projection = getMapProjection();
+  var svg = createMapBaseImage();
+
+  loadCrimeData(function(data) {
+    $.each(data, function(index, entry) {
+      var coords = projection(entry["Location"]);
+      createCircleAtLocation(svg, coords);
+    });
   });
 }
 
 
 $(document).ready(function() {
   createMap();
-  loadCrimeData();
 });
 
 
@@ -50,9 +71,6 @@ $(document).ready(function() {
 
 
 // --- Playing with circles ---
-var originX = 200;
-var originY = 200;
-var circleRadius = 10;
 // var circle = svg.append("circle").attr({
 //     cx: originX,
 //     cy: originY,
