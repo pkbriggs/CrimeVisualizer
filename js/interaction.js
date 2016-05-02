dc068d82d105cdf7521c93990cf569ca50342577
Patrick Briggs <patkbriggs@gmail.com>
2016-05-02 00:16:48 -0700

    Code cleanup

--- js/interaction.js
+++ js/interaction.js
@@ -5,14 +5,6 @@ var width = 750,
 var CRIME_CIRCLE_RADIUS = 3;
 var PX_IN_MILE = 72.02; // thus, a radius of 144 would be 2.0mi
 
-
-// globals
-// var markers[0]["latlong_pos"] = [-122.429494, 37.798033]; // arbitrary initial position
-// var markers[0]["latlong_pos"] = [-122.442380, 37.765325]; // arbitrary initial position
-// var markers[1]["latlong_pos"] = [-122.413954, 37.780000]; // arbitrary initial position
-// var markers[0]["radius"] = PX_IN_MILE * 1.0;
-// var markers[1]["radius"] = PX_IN_MILE * 1.0;
-
 var markers = [
   { // a
     "latlong_pos": [-122.429494, 37.798033], // arbitrary initial position in [long, lat] format
@@ -71,11 +63,6 @@ function setMarkerRadius(marker, radius_in_miles) {
     markers[1]["radius"] = radius_in_miles * PX_IN_MILE;
 }
 
-function createCircle(svg, props) {
-  var circle = svg.append("circle").attr(props);
-  return circle;
-}
-
 function loadCrimeData(callback) {
   d3.json("../data/scpd_incidents.json", function(dataObject) {
     data = dataObject["data"];
@@ -84,24 +71,13 @@ function loadCrimeData(callback) {
 }
 
 function updateAAndBMarkers(svg, projection) {
+  // create drag behavior so we can attach it to the markers
   var drag = d3.behavior.drag()
     // .on('dragstart', function() { circle.style('fill', 'red'); })
     // .on('drag', function(d, i) { circle.attr('cx', d3.event.x)
                                   // .attr('cy', d3.event.y); })
+    // .on('dragend', function() { circle.style('fill', 'black'); });
     .on("drag", function(d, marker_num) {
-      // d["xy_pos"][0] += d3.event.dx;
-      // d["xy_pos"][1] += d3.event.dy;
-      // updateAAndBMarkers(svg, projection);
-      // d3.select(this).attr("transform", function(d,i) {
-      //   console.log("in transform: d=");
-      //   console.log(d);
-      //   console.log(i);
-      //   return "translate(" + [ d.x,d.y ] + ")"
-      // });
-
-      // var marker = d3.select(this);
-      // var new_x = parseInt(marker.attr("cx")) + d3.event.dx;
-      // var new_y = parseInt(marker.attr("cy")) + d3.event.dy;
       var new_x = markers[marker_num]["xy_pos"][0] + d3.event.dx;
       var new_y = markers[marker_num]["xy_pos"][1] + d3.event.dy;
 
@@ -114,22 +90,14 @@ function updateAAndBMarkers(svg, projection) {
     });
 
 
-    // .on('dragend', function() { circle.style('fill', 'black'); });
-
-
-  // var a_coords = projection(markers[0]["latlong_pos"]);
-  // var b_coords = projection(markers[1]["latlong_pos"]);
-
-
-  // console.log("about to make markers");
-  var markers_parents = svg.selectAll(".map_marker")
-  // var markers_objs = svg.select(".map_marker")
+  var marker_elems = svg.selectAll(".map_marker")
     .data(markers);
 
-  // marker_parents.enter().append("g");
+  // create the parent element
+  var markers_enter = marker_elems.enter().append("g").call(drag); // have the drag call on this (the parent) element so both the circle and image get it
 
-  var markers_enter = markers_parents.enter().append("g").call(drag); // have the drag call on this (the parent) element so both the circle and image get it
 
+  // create the circle element
   var circles = markers_enter.append("circle")
     .attr("r", function(d, i) {
       return d["radius"];
@@ -149,100 +117,21 @@ function updateAAndBMarkers(svg, projection) {
     });
 
 
-  markers_parents.exit().remove();
-
-  // console.log("markers_objs = ");
-  // console.log(markers_objs);
-
+  // create the marker image
   marker_images = markers_enter.append("image")
-    // .attr("r", 50)
     .attr("xlink:href", "../img/marker.svg")
-    // .attr("class", "marker_image")
     .attr("x", function(d, i) {
       return d["xy_pos"][0] - 32/2;
     })
     .attr("y", function(d, i) {
       return d["xy_pos"][1] - 32/2;
     })
-    // .attr("fill", "black")
     .attr("height", 32)
     .attr("width", 32)
-    .attr("class", "map_marker");
+    .attr("class", "map_marker_image");
 
-
-  // svg.selectAll(".map_marker")
-  //   .append("circle")
-  //   .attr("r", 20)
-  //   .attr("cx", 0)
-  //   .attr("cy", 0)
-  //   .attr("fill", "black");
-
-
-
-
-
-
-  // var circle_a_props = {
-  //   r: markers[0]["radius"],
-  //   cx: markers[0]["xy_pos"][0],
-  //   cy: markers[0]["xy_pos"][1],
-  //   fill: "red",
-  //   opacity: 0.3
-  // };
-  // // createCircle(svg, circle_a_props);
-  // svg.append("circle").attr(circle_a_props).call(drag);
-
-  // var circle_b_props = {
-  //   r: markers[1]["radius"],
-  //   cx: markers[1]["xy_pos"][0],
-  //   cy: markers[0]["xy_pos"][1],
-  //   fill: "green",
-  //   opacity: 0.3
-  // };
-  // // createCircle(svg, circle_b_props);
-  // svg.append("circle").attr(circle_b_props).call(drag);
-
-  // shows the overlap between the two circles
-  // var interPoints = intersection(a_coords[0], a_coords[1], AB_CIRCLE_RADIUS, b_coords[0], b_coords[1], AB_CIRCLE_RADIUS);
-  // // credit: http://stackoverflow.com/questions/33330074/d3-js-detect-intersection-area
-  // svg.append("g")
-  //   .append("path")
-  //   .attr("d", function() {
-  //     return "M" + interPoints[0] + "," + interPoints[2] + "A" + AB_CIRCLE_RADIUS + "," + AB_CIRCLE_RADIUS +
-  //       " 0 0,1 " + interPoints[1] + "," + interPoints[3]+ "A" + AB_CIRCLE_RADIUS + "," + AB_CIRCLE_RADIUS +
-  //       " 0 0,1 " + interPoints[0] + "," + interPoints[2];
-  //   })
-  //   .style('fill', 'purple');
-}
-
-function addAllCrimeDataToMap(data, svg, projection) {
-  // var crime_circles = svg.selectAll("circle")
-  //   .data(data)
-  //   .enter()
-  //   .append("circle")
-  //   .attr("r", CRIME_CIRCLE_RADIUS)
-  //   .attr("fill", "white")
-  //   .attr("stroke", "black");
-
-  // crime_circles.attr("cx", function(d) {
-  //     projection(d["Location"])[0]
-  //   }).attr("cy", function(d) {
-  //     projection(d["Location"])[1]
-  //   });
-
-
-  // circle_props = {
-  //   r: CRIME_CIRCLE_RADIUS,
-  //   fill: "white",
-  //   "stroke": "black"
-  // };
-
-  // $.each(data, function(index, entry) {
-  //   var coords = projection(entry["Location"]);
-  //   circle_props["cx"] = coords[0];
-  //   circle_props["cy"] = coords[1];
-  //   createCircle(svg, circle_props);
-  // });
+  // ensure the marker is properly removed at the end of its life
+  marker_elems.exit().remove();
 }
 
 function crimeWithinMarkers(crime_coords) {
@@ -270,21 +159,10 @@ function crimeWithinMarkers(crime_coords) {
 
 
 function addCrimeDataWithinMarkers(data, svg, projection) {
-
-  // var filtered_data = data.filter(function(entry) {
-  //   var crime_coords = projection(entry["Location"]);
-  //   return crimeWithinMarkers(crime_coords);
-  // });
-  // console.log("length of filtered data: " + filtered_data.length);
-
-
   var crime_circles = svg.selectAll(".crime_circle")
     .data(window.data_within_intersection, function(d, i) {
       return d["IncidentNumber"];
     });
-
-  // var crime_circles = svg.selectAll(".crime_circle")
-  //   .data(filtered_data);
 
   crime_circles.enter()
     .append("circle")
@@ -299,30 +177,6 @@ function addCrimeDataWithinMarkers(data, svg, projection) {
     });
 
   crime_circles.exit().remove();
-
-  // crime_circles.attr("cx", function(d) {
-  //     return projection(d["Location"])[0];
-  //   }).attr("cy", function(d) {
-  //     return projection(d["Location"])[1];
-  //   });
-
-  // circle_props = {
-  //   r: CRIME_CIRCLE_RADIUS,
-  //   fill: "white",
-  //   stroke: "black",
-  //   opacity: 0.8
-  // };
-  // var a_coords = projection(markers[0]["latlong_pos"]);
-  // var b_coords = projection(markers[1]["latlong_pos"]);
-
-  // $.each(data, function(index, entry) {
-  //   var crime_coords = projection(entry["Location"]);
-  //   if (crimeWithinMarkers(crime_coords, a_coords, b_coords)) {
-  //     circle_props["cx"] = crime_coords[0];
-  //     circle_props["cy"] = crime_coords[1];
-  //     createCircle(svg, circle_props);
-  //   }
-  // });
 }
 
 
