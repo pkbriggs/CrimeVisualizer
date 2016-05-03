de061eb63b74e0166a4f678e4d924be3e701de90
Patrick Briggs <patkbriggs@gmail.com>
2016-05-03 01:06:42 -0700

    Add number of matching crimes

--- js/interaction.js
+++ js/interaction.js
@@ -453,7 +453,14 @@ function updateVisibleCrimes(all_data, projection) {
 
     return true;
   });
+
+  $(".matching_crimes_number").text(visible_crime_data.length);
 }
+
+// var updateVisibleCrimesDebounced = debounce(function(all_data, projection) {
+//   updateVisibleCrimes(all_data, projection);
+// }, 100);
+
 
 function populateTooltip($target, $tooltip, crime_data) {
   // data we can use to populate the tooltip
@@ -523,6 +530,7 @@ function createMap() {
 
     $(".vis_container").on("updated_markers", function() {
       updateVisibleCrimes(data, projection);
+      // updateVisibleCrimesDebounced(data, projection);
       d3.selectAll("circle").remove();
       // d3.selectAll(".map_marker").remove();
       marker_images.remove();
@@ -538,3 +546,26 @@ function createMap() {
 $(document).ready(function() {
   createMap();
 });
+
+
+// utility functions
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+// source: https://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
