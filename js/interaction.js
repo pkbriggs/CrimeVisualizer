80a756f34c2fb18dd69c3f384695d1c31b4a154a
Patrick Briggs <patkbriggs@gmail.com>
2016-05-02 21:44:33 -0700

    Zoom code cleanup

--- js/interaction.js
+++ js/interaction.js
@@ -156,46 +156,47 @@ function zoomed() {
   }
 }
 
-function interpolateZoom (translate, scale) {
-    var self = this;
-    return d3.transition().duration(350).tween("zoom", function () {
-        var iTranslate = d3.interpolate(zoom.translate(), translate),
-            iScale = d3.interpolate(zoom.scale(), scale);
-        return function (t) {
-            zoom
-                .scale(iScale(t))
-                .translate(iTranslate(t));
-            zoomed();
-        };
-    });
+// source: http://bl.ocks.org/linssen/7352810
+function interpolateZoom(translate, scale) {
+  return d3.transition().duration(350).tween("zoom", function () {
+    var translate_interpolation = d3.interpolate(zoom.translate(), translate),
+      scale_interpolation = d3.interpolate(zoom.scale(), scale);
+    return function (t) {
+      zoom
+        .scale(translate_interpolation(t))
+        .translate(scale_interpolation(t));
+      zoomed();
+    };
+  });
 }
 
+// source: http://bl.ocks.org/linssen/7352810
 function zoomClick() {
-    var clicked = d3.event.target,
-        direction = 1,
-        factor = 0.2,
-        target_zoom = 1,
-        center = [width / 2, height / 2],
-        extent = zoom.scaleExtent(),
-        translate = zoom.translate(),
-        translate0 = [],
-        l = [],
-        view = {x: translate[0], y: translate[1], k: zoom.scale()};
+  var clicked = d3.event.target,
+      direction = 1,
+      factor = 0.2,
+      target_zoom = 1,
+      center = [width / 2, height / 2],
+      extent = zoom.scaleExtent(),
+      translate = zoom.translate(),
+      translate0 = [],
+      l = [],
+      view = {x: translate[0], y: translate[1], k: zoom.scale()};
 
-    d3.event.preventDefault();
-    direction = (this.id === 'zoom_in') ? 1 : -1;
-    target_zoom = zoom.scale() * (1 + factor * direction);
+  d3.event.preventDefault();
+  direction = (this.id === 'zoom_in') ? 1 : -1;
+  target_zoom = zoom.scale() * (1 + factor * direction);
 
-    if (target_zoom < extent[0] || target_zoom > extent[1]) { return false; }
+  if (target_zoom < extent[0] || target_zoom > extent[1]) { return false; }
 
-    translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
-    view.k = target_zoom;
-    l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
+  translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
+  view.k = target_zoom;
+  l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
 
-    view.x += center[0] - l[0];
-    view.y += center[1] - l[1];
+  view.x += center[0] - l[0];
+  view.y += center[1] - l[1];
 
-    interpolateZoom([view.x, view.y], view.k);
+  interpolateZoom([view.x, view.y], view.k);
 }
 
 function createMapBaseImage() {
